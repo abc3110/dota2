d4f6edfa993e502b49773253c72828921f6d9be5
abc3110 <670948054@qq.com>
2019-02-20 23:44:51 +0800

    购物车功能修改

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ $(function(){
                         .children("font").html();
         var title=$parent.children(".name").html();
         var price=$parent.children(".price").html();
+        //创建商品对象保存数据
         var product={
             likeNum:likeNum,
             title:title,
@@ -14,8 +15,10 @@ $(function(){
         var pArray=new Array();
         pArray.push(product);
         var pArrStr=JSON.stringify(pArray);
-        alert(pArrStr);
+        //将数据存入localStorage
         localStorage.setItem("carts",pArrStr);
         console.log(localStorage.getItem("carts"));
+        //跳转到购物车页面
+        location.href="shopcart.html";
     })
 })
--- js/shopCart.js
+++ js/shopCart.js
@@ -1,43 +1,58 @@
 $(function(){
-    var storeProducts=localStorage.getItem("carts");
-    var productArr=JSON.parse(storeProducts);
-    if(productArr==null){
-        $(".cart-null").css("display","block");
-        console.log($(".cart-null"));   
-    }else{
-        var str="";
-        for(var item of productArr){
-            str+=`<tr>
-                <td><input type="checkbox"></td>
-                <td>${item.title}</td>
-                <td>${item.price}</td>
-                <td>${item.likeNum}</td>
-                <td><a href="javascript:;" class="del"></a></td>
-            </tr>`
+    showCartProduct();
+    function showCartProduct(){
+        //获取localStorage
+        var storeProducts=localStorage.getItem("carts");
+        var productArr=JSON.parse(storeProducts);
+        //判断是否有值
+        if(productArr==null){                           //本地存储为空显示：空空如也
+            var $elem=$(".cart-list");
+            if($elem){
+                $elem.remove();
+            }
+            $(".cart-null").css("display","block");
+            console.log($(".cart-null"));   
+        }else{                                          //本地存储不为空，创建购物车表
+            $(".cart-null").css("display","none");                               
+            var str="";
+            for(var item of productArr){
+                str+=`<tr>
+                    <td><input type="checkbox"></td>
+                    <td>${item.title}</td>
+                    <td>${item.price}</td>
+                    <td>${item.likeNum}</td>
+                    <td><a href="javascript:;" class="del"></a></td>
+                </tr>`
+            }
+            var html=`<div class="cart-list">
+                        <form>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <input type="checkbox">全选
+                                        </th>
+                                        <th>商品名称</th>
+                                        <th>价格</th>
+                                        <th>关注</th>
+                                        <th>操作</th>
+                                    <tr>
+                                    ${str}
+                                </thead>
+                            </table>
+                        </form>
+                    </div>`;
+            var $elem=$(html);
+            $(".theme-content").prepend($elem);
         }
-        var html=`<div class="cart-list">
-                    <form>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>
-                                        <input type="checkbox">全选
-                                    </th>
-                                    <th>商品名称</th>
-                                    <th>价格</th>
-                                    <th>关注</th>
-                                    <th>操作</th>
-                                <tr>
-                                ${str}
-                            </thead>
-                        </table>
-                    </form>
-                </div>`;
-        var $elem=$(html);
-        $(".theme-content").prepend($elem);
     }
+    //删除按钮单击事件：
     $("a.del").click(function(){
-        alert();
         localStorage.removeItem("carts");
+        showCartProduct();
     })
+    //window.onstorage
+    window.onstorage=function(){
+        showCartProduct();
+    }
 })
