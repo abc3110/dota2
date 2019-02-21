7d50e19bb03fcd245cadd80f1de7d361803f0006
abc3110 <670948054@qq.com>
2019-02-22 05:02:09 +0800

    添加链接，编写商品详情页

--- js/details.js
+++ js/details.js
@@ -9,4 +9,24 @@ $(function(){
         $(".shoppingPopus").show();
         $ato.addClass("on");
     })
+    $(".btn_addCart").click(function(){
+        var product={
+            likeNum:19,
+            title:"DOTA2 - 扭蛋手办 II",
+            price:69.00
+        }
+        var pArray=new Array();
+        pArray.push(product);
+        var pArrayStr=JSON.stringify(pArray);
+        localStorage.setItem("carts",pArrayStr);
+        open("shopCart.html","_blank");
+    })
+    $("[data-toggle=tab]").click(function(){
+        var $li=$(this);
+        $("[data-toggle=tab]").removeClass("curr");
+        $li.addClass("curr");
+        $(".desc-pane>div").css("display","none");
+        var id=$li.attr("data-target");
+        $(id).css("display","block");
+    })
 })
