9c283f105605f7b5caaf31ea0b81a1917fc88b28
anasband <anasband@qq.com>
2019-02-23 14:12:59 +0800

    首页购物车

--- js/header.js
+++ js/header.js
@@ -1,22 +1,22 @@
 $(window).load(function(){
     /*logo栏购物车片段，显示和隐藏 */
-    $("a.mycart.top_cart_btn").mouseover(function(){
+    $("a.mycart.top_cart_btn").mouseenter(function(){
         var $mycart=$(this);
         $mycart.addClass("on");
         $("div.mycart-list.top_cart_list").css("display","block");
     })
     var n;
-    $("a.mycart.top_cart_btn").mouseout(function(){
+    $("a.mycart.top_cart_btn").mouseleave(function(){
         var $mycart=$(this);
         n=setTimeout(function(){
             $mycart.removeClass('on');
             $("div.mycart-list.top_cart_list").css("display","none");
-        },50);
+        },100);
     })
-    $("div.mycart-list.top_cart_list").mouseover(function(){
+    $("div.mycart-list.top_cart_list").mouseenter(function(){
         clearTimeout(n);
     });
-    $("div.mycart-list.top_cart_list").mouseout(function(){
+    $("div.mycart-list.top_cart_list").mouseleave(function(){
         $mycartlist=$(this);
         $("a.mycart.top_cart_btn").removeClass('on');
         $mycartlist.css("display","none");
@@ -35,6 +35,13 @@ $(window).load(function(){
         var $btnC=$(this);
         var $parent=$btnC.parent();
         $parent.css("display","none");
-
     })
+    /** 我的购物车内容显示 **/
+    var carts=localStorage.getItem("carts");
+    if(carts!=null){
+        var pArray=JSON.parse(carts);
+        //var html=`<div>${pArray[0].title}</div>`;
+        //var $div=$(html);
+        //$div.replaceAll($(".top_cart_list>p"));
+    }
 })
